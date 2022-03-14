145c1ec4a978b86bfae8f2fb6faf1e883bcae3a2
Carlos <morales22@outlook.com>
2022-03-14 14:40:46 -0500

    Initial Commit

---
+++ iOSConcha/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSConcha
+//
+//  Created by Carlos Morales III on 3/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSConcha/iOSConchaApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSConchaApp.swift
+//  iOSConcha
+//
+//  Created by Carlos Morales III on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSConchaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
